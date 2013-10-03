35aa7b9d2ceda9937b65b95323fce1ef65600f94
NathanTeeuwen <sparant@gmail.com>
2013-10-03 10:26:07 -0700

    Add an alternative bigmoneysmithy strategy.

--- TestProgram/Program.cs
+++ TestProgram/Program.cs
@@ -12,8 +12,8 @@ namespace Program
     {        
         static void Main()
         {
-            ComparePlayers(Strategies.Lookout.Player(1), Strategies.BigMoney.Player(2), useShelters: true);
-            //CompareStrategyVsAllKnownStrategies(Strategies.LookoutSalvagerLibraryHighwayFestival.Player(1));
+            ComparePlayers(Strategies.RatsWanderingMinstrelWatchtowerArmory.Player(1), Strategies.BigMoney.Player(2), useShelters:true);
+            //CompareStrategyVsAllKnownStrategies(Strategies.BigMoneySmithyTest.Player(1));
         }
 
         static void CompareStrategyVsAllKnownStrategies(PlayerAction playerAction, bool shouldParallel = true, bool useShelters = false)
--- TestProgram/Strategies/BigMoney.cs
+++ TestProgram/Strategies/BigMoney.cs
@@ -32,4 +32,54 @@ namespace Program
             }
         }                           
     }
+
+    public static partial class Strategies
+    {
+        public static class BigMoneySmithyEarlyProvince
+        {
+            public static PlayerAction Player(int playerNumber)
+            {
+                return new PlayerAction(
+                            "BigMoneySmithyEarlyProvince",
+                            playerNumber,
+                            purchaseOrder: PurchaseOrder());
+            }
+
+            private static CardPickByPriority PurchaseOrder()
+            {
+                return new CardPickByPriority(
+                           CardAcceptance.For<CardTypes.Province>(gameState => CountAllOwned<CardTypes.Gold>(gameState) > 0),
+                           CardAcceptance.For<CardTypes.Duchy>(gameState => CountOfPile<CardTypes.Province>(gameState) <= 5),
+                           CardAcceptance.For<CardTypes.Estate>(gameState => CountOfPile<CardTypes.Province>(gameState) <= 2),
+                           CardAcceptance.For<CardTypes.Gold>(),
+                           CardAcceptance.For<CardTypes.Smithy>(gameState => CountAllOwned<CardTypes.Smithy>(gameState) < 1),
+                           CardAcceptance.For<CardTypes.Silver>());
+            }
+        }
+    }
+
+    public static partial class Strategies
+    {
+        public static class BigMoneyCouncilRoomEarlyProvince
+        {
+            public static PlayerAction Player(int playerNumber)
+            {
+                return new PlayerAction(
+                            "BigMoneyCouncilRoomEarlyProvince",
+                            playerNumber,
+                            purchaseOrder: PurchaseOrder());
+            }
+
+            private static CardPickByPriority PurchaseOrder()
+            {
+                return new CardPickByPriority(
+                           CardAcceptance.For<CardTypes.Province>(gameState => CountAllOwned<CardTypes.Gold>(gameState) > 0),
+                           CardAcceptance.For<CardTypes.Duchy>(gameState => CountOfPile<CardTypes.Province>(gameState) <= 5),
+                           CardAcceptance.For<CardTypes.Estate>(gameState => CountOfPile<CardTypes.Province>(gameState) <= 2),
+                           CardAcceptance.For<CardTypes.Gold>(),                           
+                           CardAcceptance.For<CardTypes.CouncilRoom>(gameState => CountAllOwned<CardTypes.CouncilRoom>(gameState) < 2),
+                           CardAcceptance.For<CardTypes.Silver>());
+            }
+        }      
+    }
 }
