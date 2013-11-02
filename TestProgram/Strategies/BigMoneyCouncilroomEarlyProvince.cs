9171380ff0661cf8f9fad022a5b0f04311c4e536
NathanTeeuwen <sparant@gmail.com>
2013-11-01 17:58:22 -0700

    BigMoney strategy separated into files.

--- TestProgram/Strategies/BigMoneyCouncilroomEarlyProvince.cs
+++ TestProgram/Strategies/BigMoneyCouncilroomEarlyProvince.cs
@@ -5,28 +5,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace Strategies
 {
-    public class BigMoney
+    public class BigMoneyCouncilRoomEarlyProvince
         : Strategy
     {
         public static PlayerAction Player()
         {
             return new PlayerAction(
-                        "BigMoney",
+                        "BigMoneyCouncilRoomEarlyProvince",
                         purchaseOrder: PurchaseOrder());
         }
 
         private static CardPickByPriority PurchaseOrder()
         {
             return new CardPickByPriority(
-                        CardAcceptance.For(Cards.Province, gameState => CountAllOwned(Cards.Gold, gameState) > 2),
-                        CardAcceptance.For(Cards.Duchy, gameState => CountOfPile(Cards.Province, gameState) <= 4),
+                        CardAcceptance.For(Cards.Province, gameState => CountAllOwned(Cards.Gold, gameState) > 0),
+                        CardAcceptance.For(Cards.Duchy, gameState => CountOfPile(Cards.Province, gameState) <= 5),
                         CardAcceptance.For(Cards.Estate, gameState => CountOfPile(Cards.Province, gameState) <= 2),
                         CardAcceptance.For(Cards.Gold),
-                        CardAcceptance.For(Cards.Estate, gameState => CountOfPile(Cards.Province, gameState) < 4),
+                        CardAcceptance.For(Cards.CouncilRoom, gameState => CountAllOwned(Cards.CouncilRoom, gameState) < 2),
                         CardAcceptance.For(Cards.Silver));
         }
     }
 }
+
--- TestProgram/Strategies/BigMoneySmithyEarlyProvince.cs
+++ TestProgram/Strategies/BigMoneySmithyEarlyProvince.cs
@@ -5,27 +5,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace Strategies
 {
-    public class BigMoney
+    public class BigMoneySmithyEarlyProvince
         : Strategy
     {
         public static PlayerAction Player()
         {
             return new PlayerAction(
-                        "BigMoney",
+                        "BigMoneySmithyEarlyProvince",
                         purchaseOrder: PurchaseOrder());
         }
 
         private static CardPickByPriority PurchaseOrder()
         {
             return new CardPickByPriority(
-                        CardAcceptance.For(Cards.Province, gameState => CountAllOwned(Cards.Gold, gameState) > 2),
-                        CardAcceptance.For(Cards.Duchy, gameState => CountOfPile(Cards.Province, gameState) <= 4),
+                        CardAcceptance.For(Cards.Province, gameState => CountAllOwned(Cards.Gold, gameState) > 0),
+                        CardAcceptance.For(Cards.Duchy, gameState => CountOfPile(Cards.Province, gameState) <= 5),
                         CardAcceptance.For(Cards.Estate, gameState => CountOfPile(Cards.Province, gameState) <= 2),
                         CardAcceptance.For(Cards.Gold),
-                        CardAcceptance.For(Cards.Estate, gameState => CountOfPile(Cards.Province, gameState) < 4),
+                        CardAcceptance.For(Cards.Smithy, gameState => CountAllOwned(Cards.Smithy, gameState) < 1),
                         CardAcceptance.For(Cards.Silver));
         }
     }
