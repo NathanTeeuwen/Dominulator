2c62197c7380b3718e7855e1b556b2f63c3513bf
fyl <fangyl43@gmail.com>
2014-09-23 14:18:06 -0700

    Change default action for Steward

--- Dominion.Strategy/DefaultPlayRules/Cards/Steward.cs
+++ Dominion.Strategy/DefaultPlayRules/Cards/Steward.cs
@@ -13,7 +13,11 @@ namespace Dominion.Strategy.DefaultPlayRules.Cards
 
         public override PlayerActionChoice ChooseBetween(GameState gameState, IsValidChoice acceptableChoice)
         {
-            if ((Strategy.CountOfPile(Dominion.Cards.Province, gameState)>7)&&(Strategy.CountInHand(Dominion.Cards.Copper, gameState)>1))
+            if ((Strategy.CountOfPile(Dominion.Cards.Province, gameState) > 7) &&
+                    ((Strategy.CountInHand(Dominion.Cards.Copper, gameState) > 1)
+                    || (Strategy.CountInHand(Dominion.Cards.Estate, gameState) > 1)
+                   // ||((Strategy.CountInHand(Dominion.Cards.Estate, gameState) > 0) && (Strategy.CountInHand(Dominion.Cards.Copper, gameState) > 0))
+                    ))
                 return PlayerActionChoice.Trash;
             else if ((Strategy.CountInHand(Dominion.Cards.Silver, gameState) < 3) && (Strategy.CountInHand(Dominion.Cards.Gold, gameState) < 0))
                 return PlayerActionChoice.PlusCoin;
